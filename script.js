38e979ff3302185b0ad8388a67d4630055aeb361
mayonk25 <100419299+mayonk25@users.noreply.github.com>
2023-10-03 10:54:47 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let button = document.querySelector('.button')
+let input = document.querySelector('.input')
+let namakota = document.querySelector('#namakota')
+let suhukota = document.querySelector('#suhukota')
+let cuacakota = document.querySelector('#cuacakota')
+
+button.addEventListener("click",function() {
+
+    fetch(`http://api.weatherapi.com/v1/current.json?key=be2b7b035b6446f592312448230310&q=${input.value}`)
+    .then(response => response.json())
+    .then(tampilData)
+    .catch(err => alert("Nama Kota Salah"));
+    })
+
+const tampilData=(weather)=>{
+    console.log(weather)
+    namakota.innerText = `${weather.location.name}`
+    suhukota.innerHTML = `${weather.current.temp_c}&deg;C`
+    cuacakota.innerText = `${weather.current.condition.text}`
+ }
