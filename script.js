6b9f8a83979851b194a62f28672c9b58d290da84
mayonk25 <100419299+mayonk25@users.noreply.github.com>
2023-10-31 17:02:37 +0700

    fix : changed API access to https for github pages

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ let suhukota = document.querySelector('#suhukota')
 let cuacakota = document.querySelector('#cuacakota')
 
 button.addEventListener("click",function() {
-
-    fetch(`http://api.weatherapi.com/v1/current.json?key=be2b7b035b6446f592312448230310&q=${input.value}`)
+    fetch(`https://api.weatherapi.com/v1/current.json?key=be2b7b035b6446f592312448230310&q=${input.value}`)
     .then(response => response.json())
     .then(tampilData)
     .catch(err => alert("Nama Kota Salah"));
     })
 
+
 const tampilData=(weather)=>{
-    console.log(weather)
+    console.log(weather);
     namakota.innerText = `${weather.location.name}`
     suhukota.innerHTML = `${weather.current.temp_c}&deg;C`
     cuacakota.innerText = `${weather.current.condition.text}`
